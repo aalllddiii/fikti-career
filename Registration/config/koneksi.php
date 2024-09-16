1347a714a3701d14f10cb34e90fef7eb0e549318
aalllddiii <aldicahyar@gmail.com>
2024-09-16 09:46:53 +0700

    first init github

---
+++ Registration/config/koneksi.php
@@ -0,0 +1,19 @@
+<?php
+
+// $host   = 'localhost';
+// $user   = 'root';
+// $pass   = 'root';
+// $db     = 'fikti_career';
+
+$host   = 'localhost';
+$user   = 'root';
+$pass   = '';
+$db     = 'fikticar_ficar';
+
+
+
+$conn    = mysqli_connect($host, $user, $pass, $db) or die("ERROR");
+
+    // if ($con){
+    //     echo "ada koneksi ke database";
+    // }
